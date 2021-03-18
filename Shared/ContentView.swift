11deb36a12ed9294bd22882e231c05419f08afa2
consuelita <adelita@operatorfoundation.org>
2021-03-18 17:44:20 -0500

    Initial Commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+        List {
+            Text("Brandon")
+            Text("Adelita")
+        }
+            .navigationTitle("Contacts")
+            Image("FixMe")
+        }
     }
 }
 
