7cbfa2b1b0777a1a10e91c1b4bf9d9ad34269cea
consuelita <adelita@operatorfoundation.org>
2021-03-18 15:48:44 -0500

    Initial Commit

---
+++ Shared/GlassApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassApp.swift
+//  Shared
+//
+//  Created by Mafalda on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct GlassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
